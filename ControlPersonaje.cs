3db251f1ab363d833249e8f0dd66991682650dc0
Aldo Guzman <75010573+AG-jpg@users.noreply.github.com>
2022-08-27 10:33:43 +0300

    Add Max Points

---
+++ ControlPersonaje.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPersonaje : MonoBehaviour
+{
+    public Transform comienzoRayo;
+    private Animator animator;
+    private Rigidbody rb;
+    private bool caminarDerecha = true;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CambiarDireccion();
+        }
+
+        RaycastHit contacto;
+
+        if(!Physics.Raycast(comienzoRayo.position, -transform.up, out contacto, Mathf.Infinity))
+        {
+            animator.SetTrigger("Cayendo");
+        }
+
+        if(transform.position.y < -2)
+        {
+            gameManager.FinalizarJuego();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(!gameManager.juegoIniciado)
+        {
+            return;
+        }else
+        {
+            animator.SetTrigger("ComienzaJuego");
+        }
+
+        rb.transform.position = transform.position + transform.forward * 2 * Time.deltaTime;
+    }
+
+    private void CambiarDireccion()
+    {
+        if(!gameManager.juegoIniciado)
+        {
+            return;
+        }
+
+        caminarDerecha = !caminarDerecha;
+
+        if(caminarDerecha)
+        {
+            transform.rotation = Quaternion.Euler(0,45,0);
+        } else
+        {
+            transform.rotation = Quaternion.Euler(0,-45,0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag == "Crystal")
+        {
+            Destroy(other.gameObject);
+            gameManager.AumentarPuntaje();
+        }
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public bool juegoIniciado;
+    public int puntaje;
+    public Text textoPuntaje;
+    public Text puntajeMaximoTexto;
+
+    private void Awake() 
+    {
+        //Obtener puntaje máximo
+        puntajeMaximoTexto.text = "Best: " + ObtenerPuntajeMaximo().ToString();
+    }
+
+    public void IniciarJuego()
+    {
+        juegoIniciado = true;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            IniciarJuego();
+        }    
+    }
+
+    public void FinalizarJuego()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void AumentarPuntaje()
+    {
+        puntaje++;
+        textoPuntaje.text = puntaje.ToString();
+
+        if(puntaje > ObtenerPuntajeMaximo())
+        {
+            PlayerPrefs.SetInt("PuntajeMaximo", puntaje);
+            puntajeMaximoTexto.text = "Best: " + puntaje.ToString();
+        }
+    }
+
+    public int ObtenerPuntajeMaximo()
+    {
+        int i = PlayerPrefs.GetInt("PuntajeMaximo");
+        return i;
+    }
+}
